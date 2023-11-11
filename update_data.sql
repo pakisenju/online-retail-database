43ce29cd906d58eeb3411f91b933c34917ff1788
Fahki Rohandi <fahkirohandi07@gmail.com>
2023-11-12 00:06:49 +0800

    mengubah data pada database

---
+++ update_data.sql
@@ -0,0 +1,3 @@
+UPDATE Pelanggan SET nomor_telepon = '08217638792' WHERE id_pelanggan = 2;
+UPDATE Barang SET harga = 130000 WHERE id_barang = 3;
+UPDATE Pesanan SET jumlah_barang = 4 WHERE id_pesanan = 5;
